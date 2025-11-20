cf730a2e9dd1ec3bd844c411d7a550384391997b
Prakash Kumar <prakashpandeyec032@gmail.com>
2025-11-20 12:26:08 +0530

    issue fix

--- src/main/java/com/smartgaon/ai/smartgaon_api/gaonconnect/problemreport/ProblemReportController.java
+++ src/main/java/com/smartgaon/ai/smartgaon_api/gaonconnect/problemreport/ProblemReportController.java
@@ -3,12 +3,13 @@ package com.smartgaon.ai.smartgaon_api.gaonconnect.problemreport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smartgaon.ai.smartgaon_api.model.ProblemReport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -21,6 +22,10 @@ public class ProblemReportController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private ProblemReportRepository reportRepo;
+
+
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createReport(
             @RequestPart("data") String data,
@@ -31,5 +36,27 @@ public class ProblemReportController {
         ProblemReport saved = service.createReport(req, files);
         return ResponseEntity.ok(saved);
     }
+
+    @GetMapping("/page")
+    public ResponseEntity<?> getPaginatedReports(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+
+        Page<ProblemReport> pageResult = reportRepo.findAll(pageable);
+
+        return ResponseEntity.ok(pageResult);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getReportById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                reportRepo.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Report not found"))
+        );
+    }
+
 }
 
